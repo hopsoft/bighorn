95689c9524b1fd3f6190c59fc402f956ab91289b
Nathan Hopkins <natehop@gmail.com>
2015-12-10 13:19:39 -0500

    Strip reserver chars from keys & values

--- src/kvn.js
+++ src/kvn.js
@@ -1,6 +1,10 @@
 var util = require("./util");
 var enumerable = require("./enumerable");
 
+var keyValueDelim = ":";
+var pairDelim = "; ";
+var reservedPattern = new RegExp(keyValueDelim + "|" + pairDelim, "g");
+
 /**
  * Converts a value into a KVN (Key Value Notation) string value
  * KVN stores key/value pairs as a string... think of it as a subset of JSON
@@ -21,15 +25,12 @@ module.exports = function (value) {
     return String(value);
   }
 
-  var keyValueDelim = ":";
-  var pairDelim = "; ";
-
   var keys = enumerable.reduce(value, function (key, _, memo) {
     memo.push(key);
   }, []).sort();
 
   var flattened = enumerable.map(keys, function (key) {
-    return String(key) + keyValueDelim + String(value[key]);
+    return String(key).replace(reservedPattern, "") + keyValueDelim + String(value[key]).replace(reservedPattern, "");
   });
 
   return flattened.join(pairDelim);
