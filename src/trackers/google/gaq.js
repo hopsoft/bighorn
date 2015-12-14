766bbc0a1a3538f965263df81252590ad3dfb7db
Nathan Hopkins <natehop@gmail.com>
2015-12-14 10:10:50 -0500

    Added gaq tracking for google

---
+++ src/trackers/google/gaq.js
@@ -0,0 +1,20 @@
+var util = require("../../util");
+var kvn = require("../../kvn");
+
+module.exports = function (category, action, label, value) {
+  try {
+    category = kvn(category);
+    action   = kvn(action);
+    label    = kvn(label);
+
+    if (!util.isValidString(category)) { return; }
+    if (!util.isValidString(action)) { return; }
+    if (!util.isObject(self._gaq) || !util.isFunction(self._gaq.push)) { return; }
+    if (util.isFunction(self.ga)) { return; } // let ga manage the tracking
+
+    self._gaq.push(["trackEvent", category, action, label, value]);
+    console.log("SUCCESS Bighorn.track piwik", category, action, label, value);
+  } catch (e) {
+    console.log("ERROR Bighorn.track piwik", category, action, label, value, e.message);
+  }
+};
